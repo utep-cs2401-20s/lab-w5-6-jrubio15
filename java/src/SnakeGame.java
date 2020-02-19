e56e239477a590e31c94e14e39bce5e1d92ad556
Javier Rubio <jrubio15@miners.utep.edu>
2020-02-19 14:44:30 -0700

    Confused on setting up the constructor.

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -1,2 +1,21 @@
 public class SnakeGame {
+    // Attributes
+    private boolean[][] game;
+    private int headPosition;
+    private static int exhaustiveChecks;
+    private static int recursiveChecks;
+    //Constructors
+    public
+
+  //      public int[] findTailExhaustive () {}
+
+
+    {
+        public int[] findTailRecursive()
+  //      public int[] findTailRecursive ( int[] currentPosition, int[] previousPosition);
+    }
+
+//    private void resetCounters();
 }
+
+
