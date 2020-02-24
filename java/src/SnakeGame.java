d1ed14af04197b63995f716fdcbc35bd1a0aca66
Javier Rubio <jrubio15@miners.utep.edu>
2020-02-24 14:51:33 -0700

    Setting Up the board.

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -5,14 +5,24 @@ public class SnakeGame {
     private static int exhaustiveChecks;
     private static int recursiveChecks;
     //Constructors
-    public
-
-  //      public int[] findTailExhaustive () {}
+    public SnakeGame(){
+        boolean [][] emptyBoard = {};
+    }
+    public SnakeGame(boolean[][] board, int x, int y){
+        for(int i = 0; i < board.length; i++ ){
+            for(int j = 0; j < board.length; j++){
+                game[i][j] = board[i][j];
+            }
+        }
+        headPosition[0] = x;
+        headPosition[1] = y;
+    }
+    //      public int[] findTailExhaustive () {}
 
 
     {
         public int[] findTailRecursive()
-  //      public int[] findTailRecursive ( int[] currentPosition, int[] previousPosition);
+        //      public int[] findTailRecursive ( int[] currentPosition, int[] previousPosition);
     }
 
 //    private void resetCounters();
