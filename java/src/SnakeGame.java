6c2117c212886c5c97097c33ba418f8b0d24302e
Javier Rubio Jr <jrubio15@miners.utep.edu>
2020-02-29 09:14:24 -0700

    Changed the method and little tweaks on the Attributes

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -1,21 +1,24 @@
 public class SnakeGame {
     // Attributes
-    private boolean[][] game;
-    private int[] headPosition;
+    private boolean game [][];
+    private int headPosition[];
     public static int exhaustiveChecks;
     public static int recursiveChecks;
+    public SnakeGame(){
+        this.game = new boolean[1][1];
+        this.exhaustiveChecks = 0;
+        this.recursiveChecks = 0;
+    }
     //Constructors
     public SnakeGame(){
         boolean [][] emptyBoard = {};
     }
-    public SnakeGame(boolean[][] board, int x, int y){
-        for(int i = 0; i < board.length; i++ ){
-            for(int j = 0; j < board.length; j++){
-                game[i][j] = board[i][j];
-            }
-        }
-        headPosition[0] = x;
-        headPosition[1] = y;
+    public SnakeGame(boolean[][] game, int x, int y){
+        this.game = game;
+        headPosition [0] = x;
+        headPosition [1]= y;
+        this.exhaustiveChecks = 0;
+        this.recursiveChecks = 0;
     }
     // Methods
     public int neigh(int x, int y){
@@ -41,6 +44,3 @@ public class SnakeGame {
     public int[] findTailExhaustive()
 
 }
-
-
-
